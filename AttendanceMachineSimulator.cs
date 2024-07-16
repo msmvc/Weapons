ac1c29de00c7bb74eb2c324de589e091afd34478
msmvc <msmvc@163.com>
2024-07-16 18:07:31 +0900

    Add files via upload

---
+++ AttendanceMachineSimulator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceMachineSimulator
+{
+	class AttendanceMachineSimulator
+	{
+        private readonly RestClientHelper _clientHelper;
+
+        public AttendanceMachineSimulator(string baseUrl)
+        {
+            _clientHelper = new RestClientHelper(baseUrl);
+        }
+
+        public async Task CheckIn(string employeeId)
+        {
+            var response = await _clientHelper.PostAsync("/attendance/checkin", new { EmployeeId = employeeId, Timestamp = DateTime.UtcNow });
+            if (response.IsSuccessful)
+            {
+                Console.WriteLine("Check-in successful");
+            }
+            else
+            {
+                Console.WriteLine("Check-in failed: " + response.ErrorMessage);
+            }
+        }
+
+        public async Task CheckOut(string employeeId)
+        {
+            var response = await _clientHelper.PostAsync("/attendance/checkout", new { EmployeeId = employeeId, Timestamp = DateTime.UtcNow });
+            if (response.IsSuccessful)
+            {
+                Console.WriteLine("Check-out successful");
+            }
+            else
+            {
+                Console.WriteLine("Check-out failed: " + response.ErrorMessage);
+            }
+        }
+    }
+}
---
+++ RestClientHelper.cs
@@ -0,0 +1,41 @@
+﻿using RestSharp;
+using System.Threading.Tasks;
+
+namespace AttendanceMachineSimulator
+{
+	public class RestClientHelper
+	{
+        private readonly RestClient _client;
+
+        public RestClientHelper(string baseUrl)
+        {
+            _client = new RestClient(baseUrl);
+        }
+
+        public async Task<RestResponse> GetAsync(string endpoint)
+        {
+            var request = new RestRequest(endpoint, Method.Get);
+            return await _client.ExecuteAsync(request);
+        }
+
+        public async Task<RestResponse> PostAsync(string endpoint, object body)
+        {
+            var request = new RestRequest(endpoint, Method.Post);
+            request.AddJsonBody(body);
+            return await _client.ExecuteAsync(request);
+        }
+
+        public async Task<RestResponse> PutAsync(string endpoint, object body)
+        {
+            var request = new RestRequest(endpoint, Method.Put);
+            request.AddJsonBody(body);
+            return await _client.ExecuteAsync(request);
+        }
+
+        public async Task<RestResponse> DeleteAsync(string endpoint)
+        {
+            var request = new RestRequest(endpoint, Method.Delete);
+            return await _client.ExecuteAsync(request);
+        }
+    }
+}
